2c1612e5a8831ae53934d58cb6f7de40ac0fc516
frankzhuao123 <91228378+frankzhuao123@users.noreply.github.com>
2024-10-03 22:13:45 -0400

    configured servers

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const webpack = require('webpack');
 
 module.exports = {
+  devServer: {
+    allowedHosts: ['lucid-labs.xyz', 'www.lucid-labs.xyz','https://wrldweb.up.railway.app/','https://lucidlabs.up.railway.app/'],
+  },
   configureWebpack: {
     plugins: [
       new webpack.ProgressPlugin()
