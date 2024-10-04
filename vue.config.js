a0f4939e3ff2608cbe48d8abaa035f2adcaf806f
frankzhuao123 <91228378+frankzhuao123@users.noreply.github.com>
2024-10-03 22:20:50 -0400

    configured servers

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 
 module.exports = {
   devServer: {
-    allowedHosts: ['lucid-labs.xyz', 'www.lucid-labs.xyz','https://wrldweb.up.railway.app/','https://lucidlabs.up.railway.app/'],
+    allowedHosts: ['.lucid-labs.xyz', 'www.lucid-labs.xyz','https://wrldweb.up.railway.app/','https://lucidlabs.up.railway.app/'],
   },
   configureWebpack: {
     plugins: [
